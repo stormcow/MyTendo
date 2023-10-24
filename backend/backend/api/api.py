bb14651c7ab8601e20637c311edcb13233d38b6e
stormcow <monkiimode@gmail.com>
2023-10-24 22:01:16 +0200

    Added endpoint for getting all switch ex games

---
+++ backend/backend/api/api.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+from fastapi import APIRouter, status
+
+from ..service import handleWiki
+
+router = APIRouter(prefix="/api", tags=["api"])
+
+
+@router.get(path="/test", tags=["test"])
+async def get_test() -> dict[str, str | int]:
+    return {"message": "OKAY", "status": status.HTTP_200_OK}
+
+
+@router.get(path="/games", tags=["games"])
+async def get_games() -> dict[str, Any]:
+    try:
+        games = await handleWiki.get_all_games()
+        return {
+            "message": "Okay",
+            "games": games,
+            "amount_of_games": len(games),
+            "status": status.HTTP_200_OK,
+        }
+    except Exception as e:
+        return {"message": e}
---
+++ backend/backend/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+from .api import api
+
+app = FastAPI()
+
+app.include_router(api.router)
